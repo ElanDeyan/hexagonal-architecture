4030a8a2f940f680719f12de9f3b23af724a2651
Elan Deyan <deyancaio83@hotmail.com>
2023-12-31 21:58:31 -0300

    style: formats files

--- src/app/menu/mainMenu.ts
+++ src/app/menu/mainMenu.ts
@@ -1,23 +1,22 @@
-import UsefulTerminal from "@/utils/UsefulTerminal"
-import { terminal } from "terminal-kit"
-import foundationsMenu from "./foundationsMenu"
+import UsefulTerminal from "@/utils/UsefulTerminal";
+import { terminal } from "terminal-kit";
+import foundationsMenu from "./foundationsMenu";
 
 export default async function mainMenu() {
-    UsefulTerminal.title('Main menu')
+  UsefulTerminal.title("Main menu");
 
-    const userChoice = await terminal.singleColumnMenu([
-        '1. Foundations',
-        'Exit'
-    ]).promise
+  const userChoice = await terminal.singleColumnMenu(["1. Foundations", "Exit"])
+    .promise;
 
-    switch (userChoice.selectedIndex) {
-        case 0:
-            await foundationsMenu()
-            break;
-        case 1: process.exit(0)
-        default:
-            break;
-    }
+  switch (userChoice.selectedIndex) {
+    case 0:
+      await foundationsMenu();
+      break;
+    case 1:
+      process.exit(0);
+    default:
+      break;
+  }
 
-    mainMenu()
+  mainMenu();
 }
--- src/utils/UsefulTerminal.ts
+++ src/utils/UsefulTerminal.ts
@@ -1,10 +1,10 @@
-import { terminal } from 'terminal-kit'
+import { terminal } from "terminal-kit";
 
 export default class UsefulTerminal {
-    static title(text: string) {
-        terminal.clear()
+  static title(text: string) {
+    terminal.clear();
 
-        terminal.magenta(`${text}\n`)
-        terminal.magenta(`${"-".repeat(text.length)}\n`);
-    }
+    terminal.magenta(`${text}\n`);
+    terminal.magenta(`${"-".repeat(text.length)}\n`);
+  }
 }
