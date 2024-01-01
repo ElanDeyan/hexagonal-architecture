1d56921e5f78097259576a5b6ffe2dbecaea4677
Elan Deyan <deyancaio83@hotmail.com>
2023-12-31 22:37:48 -0300

    Revert "feat: adds confrimation static function to UsefulTerminal"
    This reverts commit f64b0468980f0976837a79abec8634252ea43a23.
    
    Reason: this change could, and makes more sense, have been made in the polymorphism feature branch

--- src/app/utils/UsefulTerminal.ts
+++ src/app/utils/UsefulTerminal.ts
@@ -7,15 +7,4 @@ export default class UsefulTerminal {
     terminal.magenta(`${text}\n`);
     terminal.magenta(`${"-".repeat(text.length)}\n`);
   }
-
-  static async confirmation (text: string): Promise<boolean> {
-    terminal.yellow(`\n${text}`);
-
-    const answer = await terminal.singleLineMenu([
-      "Yes",
-      "No"
-    ]).promise
-
-    return answer.selectedIndex === 0
-  }
 }
