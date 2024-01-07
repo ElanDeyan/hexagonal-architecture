3a030f4365b0b1cb7201f4006529e874dc777aa2
Elan Deyan <deyancaio83@hotmail.com>
2024-01-07 14:39:37 -0300

    feat: adds DIP option to the foundations menu

--- src/app/menu/foundationsMenu.ts
+++ src/app/menu/foundationsMenu.ts
@@ -1,19 +1,21 @@
 import UsefulTerminal from "@/app/utils/UsefulTerminal";
 import polymorphism from "../foundations/polymorphism";
+import dip from "../foundations/dip";
 
 export default async function foundationsMenu() {
   UsefulTerminal.title("Foundations");
 
-  const [index] = await UsefulTerminal.menu(["1. Polymorphism", "Back"]);
+  const [index] = await UsefulTerminal.menu(["1. Polymorphism", "2. Dependency inversion principle", "Back"]);
 
   switch (index) {
     case 0:
         await polymorphism()
         break;
     case 1:
-      return;
+        await dip()
+        break;
     default:
-      break;
+      return;
   }
 
   await foundationsMenu();
