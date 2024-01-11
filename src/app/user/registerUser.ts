a5dc40962605d6791b7536bfff3823bded47648d
Elan Deyan <deyancaio83@hotmail.com>
2024-01-10 21:51:37 -0300

    feat: adds a simple "screen" for the registerUser use case

---
+++ src/app/user/registerUser.ts
@@ -0,0 +1,7 @@
+import UsefulTerminal from "../utils/UsefulTerminal";
+
+export default async function registerUser() {
+    UsefulTerminal.title("Register user")
+
+    await UsefulTerminal.waitEnter()
+}
