3abaa2723ad9b9fa82a0b0f29658aa2795c10e62
Elan Deyan <deyancaio83@hotmail.com>
2024-01-10 21:46:47 -0300

    feat: adds User menu

--- src/app/menu/mainMenu.ts
+++ src/app/menu/mainMenu.ts
@@ -1,19 +1,21 @@
 import UsefulTerminal from "@/app/utils/UsefulTerminal";
 import foundationsMenu from "./foundationsMenu";
+import userMenu from "./userMenu";
 
 export default async function mainMenu() {
   UsefulTerminal.title("Main menu");
 
-  const [index] = await UsefulTerminal.menu(["1. Foundations", "Exit"]);
+  const [index] = await UsefulTerminal.menu(["1. Foundations", "2. User", "Exit"]);
 
   switch (index) {
     case 0:
       await foundationsMenu();
       break;
     case 1:
-      process.exit(0);
-    default:
+      await userMenu();
       break;
+    default:
+      process.exit(0);
   }
 
   mainMenu();
